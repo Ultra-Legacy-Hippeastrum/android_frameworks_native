a926321b1b63c1973a81ae9a9cca0a7a351748f0
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-10-09 20:28:56 +0530

    SF: Bring back support for disabling backpressure propagation
    [Pulkit077]: Adapt to A14 QPR1
    [SamarV-121]: Adapt to A14 QPR3
    
    Taken from CLO (QSSI 13). Some Qualcomm devices can still
    benefit from disabling backpressure propagation by setting:
    
    debug.sf.disable_backpressure=1
    
    Change-Id: I669a6059a2a971aa79603e74153fa93729f703dc
    Signed-off-by: Pulkit077 <pulkitagarwal2k1@gmail.com>

--- services/surfaceflinger/SurfaceFlinger.h
+++ services/surfaceflinger/SurfaceFlinger.h
@@ -1376,6 +1376,7 @@ private:
     std::atomic_bool mForceFullDamage = false;
 
     bool mLayerCachingEnabled = false;
+    bool mPropagateBackpressure = true;
     bool mBackpressureGpuComposition = false;
 
     LayerTracing mLayerTracing;
